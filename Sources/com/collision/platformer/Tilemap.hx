121bb3209f5b03f597846993fe0fe1bd4c0b5057
Joaquin Bello <juakob@gmail.com>
2019-10-05 14:38:16 -0300

    add scale to Tilemap.hx

--- Sources/com/collision/platformer/Tilemap.hx
+++ Sources/com/collision/platformer/Tilemap.hx
@@ -11,7 +11,7 @@ class Tilemap {
     public function new() {
         
     }
-    public function init(tmxData:String,tilesImg:String,tileWidth:Int,tileHeight:Int,displayLayer:Layer):CollisionTileMap {
+    public function init(tmxData:String,tilesImg:String,tileWidth:Int,tileHeight:Int,displayLayer:Layer,scale:Float=1):CollisionTileMap {
 		var r:format.tmx.Reader = new format.tmx.Reader();
 		var t:TmxMap = r.read(Xml.parse(Assets.blobs.get(tmxData).toString()));
 		var collision:CollisionTileMap=null;
@@ -24,12 +24,12 @@ class Tilemap {
 							for(tile in tileMap.data.tiles){
 								tiles.push(tile.gid);
 							}
-							collision=new CollisionTileMap(tiles,tileWidth,tileHeight,tileMap.width, tileMap.height ) ;
+							collision=new CollisionTileMap(tiles,tileWidth*scale,tileHeight*scale,tileMap.width, tileMap.height ) ;
 					//	}
 						var tiles:Array<TmxTile> = cast tileMap.data.tiles;
 						//tileMap.data.
 						var tileMapDisplay:TileMapDisplay = new TileMapDisplay(tilesImg, tileMap.width, tileMap.height, tileWidth, tileHeight);
-						
+						tileMapDisplay.scaleX=tileMapDisplay.scaleY=scale;
 						displayLayer.addChild(tileMapDisplay);
 						var counter:Int = 0;
 
