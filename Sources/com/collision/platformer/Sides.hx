02ba251eb1ee191c5d309ff32d247f12c8b7023b
Joaquin Bello <juakob@gmail.com>
2019-10-07 01:53:49 -0300

    fix initialization

--- Sources/com/collision/platformer/Sides.hx
+++ Sources/com/collision/platformer/Sides.hx
@@ -5,10 +5,10 @@ package com.collision.platformer;
  */
 class Sides
 {
-	public static var ALL:Int=LEFT|RIGHT|TOP|BOTTOM;
-	public static var NONE:Int = 0x00000;
-	public static var LEFT:Int = 0x00001;
-	public static var RIGHT:Int = 0x00002;
-	public static var TOP:Int = 0x00004;
-	public static var BOTTOM:Int = 0x00008;
+	public inline static var NONE:Int = 0x00000;
+	public inline static var LEFT:Int = 0x00001;
+	public inline static var RIGHT:Int = 0x00002;
+	public inline static var TOP:Int = 0x00004;
+	public inline static var BOTTOM:Int = 0x00008;
+	public inline static var ALL:Int=LEFT|RIGHT|TOP|BOTTOM;
 }
