b52aca3f1f4764ebaa5bd98994e264a079211a60
Joaquin Bello <juakob@gmail.com>
2020-05-14 16:04:17 -0300

    add html5 function to strech the canvas

---
+++ Sources/com/target/Html5.hx
@@ -0,0 +1,32 @@
+package com.target;
+#if (kha_html5 && js)
+import js.html.CanvasElement;
+import js.Browser.document;
+import js.Browser.window;
+import js.Browser.navigator;
+#end
+class Html5 {
+    public static inline function hotReload(){
+        #if hotml new hotml.Client(); #end
+    }
+    public static inline function fillScreen(){
+        #if (kha_html5 && js)
+		//make html5 canvas resizable
+		document.documentElement.style.padding = "0";
+		document.documentElement.style.margin = "0";
+		document.body.style.padding = "0";
+		document.body.style.margin = "0";
+		var canvas:CanvasElement = cast(document.getElementById("khanvas"), CanvasElement);
+		canvas.style.display = "block";
+
+		var resize = function() {
+			canvas.width = Std.int(window.innerWidth * window.devicePixelRatio);
+			canvas.height = Std.int(window.innerHeight * window.devicePixelRatio);
+			canvas.style.width = document.documentElement.clientWidth + "px";
+			canvas.style.height = document.documentElement.clientHeight + "px";
+		}
+		window.onresize = resize;
+		resize();
+		#end
+    }
+}
