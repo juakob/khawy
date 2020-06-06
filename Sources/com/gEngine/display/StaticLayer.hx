8c3ca366113b9c88f49b4089f13e2ac9936863cb
Joaquin Bello <juakob@gmail.com>
2020-06-06 05:32:30 -0300

    use orthogonal instead of screenTransform

--- Sources/com/gEngine/display/StaticLayer.hx
+++ Sources/com/gEngine/display/StaticLayer.hx
@@ -4,13 +4,18 @@ import com.gEngine.painters.PaintMode;
 import kha.math.FastMatrix4;
 
 class StaticLayer extends Layer {
-	var identity:FastMatrix4 = FastMatrix4.identity();
+	var offset:FastMatrix4 ;
+	public function new() {
+		super();
+		offset= FastMatrix4.identity();
+	}
 
 	override function render(paintMode:PaintMode, transform:FastMatrix4) {
 		paintMode.render();
 		var proj=paintMode.camera.projection;
-		paintMode.camera.projection=paintMode.camera.screenTransform;
-		super.render(paintMode, identity);
+		paintMode.camera.projection = paintMode.camera.orthogonal;
+		offset.setFrom(FastMatrix4.translation(-paintMode.camera.width*0.5,-paintMode.camera.height*0.5,0));
+		super.render(paintMode, offset);
 		paintMode.render();
 		paintMode.camera.projection=proj;
 	}
