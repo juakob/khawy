d3fb0811b048dbbf2a5f30dc74fde7d7ee73c21f
Joaquin Bello <juakob@gmail.com>
2020-07-15 23:58:08 -0300

    update outline shader

--- Shaders/Shaders/outline.frag.glsl
+++ Shaders/Shaders/outline.frag.glsl
@@ -13,12 +13,18 @@ out vec4 FragColor;
 void kore() {
 	vec4 base= texture( tex, texCoord);
 	float alpha1 = texture( tex, texCoord + vec2(0,stepSize.y)).a;
-    	alpha1 -= texture( tex, texCoord + vec2(0,-stepSize.y)).a;
-	alpha1=abs(alpha1);
+	float alpha3=abs(alpha1-base.a);
+    float alpha1_ = texture( tex, texCoord + vec2(0,-stepSize.y)).a;
+	alpha1=abs(alpha1-alpha1_);
+	float alpha5=abs(alpha1_-base.a);
 	float alpha2 = texture( tex, texCoord + vec2(stepSize.x,0)).a;
-    	alpha2 -= texture( tex, texCoord + vec2(-stepSize.x,0)).a;
-	alpha2=abs(alpha2);
-	float alpha=clamp(alpha1+alpha2,0,1);
+	float alpha4=abs(alpha2-base.a);
+    float alpha2_ = texture( tex, texCoord + vec2(-stepSize.x,0)).a;
+	alpha2=abs(alpha2-alpha2_);
+	float alpha6=abs(alpha2_-base.a);
+	
+	
+	float alpha=clamp(alpha1+alpha2+alpha3+alpha4+alpha5+alpha6,0,1);
 	//float inverse=1-alpha;
     	// calculate resulting color
 		FragColor = vec4( base.rgb+color*alpha, base.a+alpha);
