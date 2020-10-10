2b1978d2d30c544d5fae72504f618eb5fcfb4769
Joaquin Bello <juakob@gmail.com>
2020-10-10 04:34:38 -0300

    update highpass

--- Shaders/Shaders/HighPassFilter.frag.glsl
+++ Shaders/Shaders/HighPassFilter.frag.glsl
@@ -5,17 +5,21 @@ precision mediump float;
 #endif
 
 uniform sampler2D tex;
+uniform vec3 brightIndex;
+uniform vec3 darkColor;
+uniform vec3 tintColor;
+uniform float tolerance;
 in vec2 texCoord;
 out vec4 FragColor;
 
 void kore() {
     vec4 color = texture( tex, texCoord );
     // check whether fragment output is higher than threshold, if so output as brightness color
-    float brightness = dot(color.rgb, vec3(1, 0.7152, 0.0));
-    if(brightness > 0.8)
+    float brightness = dot(color.rgb, brightIndex);
+    if(brightness > tolerance)
         FragColor = vec4(color.rgb, 1.0);
     else
-        FragColor = vec4(0.0, 0.0, 0.1, 1.0);
+        FragColor = vec4(darkColor.rgb, 1.0);
     
-    FragColor*= vec4(0.2, 0.3, 0.7, 1.0);
+    FragColor*= vec4(tintColor.rgb, 1.0);
 }
