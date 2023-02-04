7336659e1446ab643f7b8b1650fd3287793a5ecc
Joaquin Bello <juakob@gmail.com>
2023-02-04 18:22:46 -0300

    make bake layer wokr

--- Sources/com/gEngine/display/BakeLayer.hx
+++ Sources/com/gEngine/display/BakeLayer.hx
@@ -1,5 +1,6 @@
 package com.gEngine.display;
 
+import kha.Image;
 import kha.Color;
 import kha.math.FastVector4;
 import com.gEngine.painters.IPainter;
@@ -14,29 +15,30 @@ class BakeLayer extends Layer {
 	var width:Int = 0;
 	var height:Int = 0;
 	var scale:Float = 1;
+	var flushDrawables:Bool;
 
-	public function bake(resolution:Float = 1, width:Int = -1, height:Int = -1) {
-		if (width < 0 || height < 0) {
+	public function new(resolution:Float = 1, width:Int = -1, height:Int = -1) {
+		this.width = Math.ceil(width * resolution);
+		this.height = Math.ceil(height * resolution);
+		this.scale = resolution;
+		super();
+	}
+
+	public function bake(flashDrawables:Bool) {
+		if (width <= 0 || height <= 0) {
 			var area = new MinMax();
 			getDrawArea(area, FastMatrix4.identity());
 			width = Math.ceil(area.width());
 			height = Math.ceil(area.height());
 		}
-		this.width = Math.ceil(width * resolution);
-		this.height = Math.ceil(height * resolution);
-		this.scale = resolution;
 
-		if (renderCamera != null) {
-			if (renderCamera.width != this.width || renderCamera.height != this.height) {
-				renderCamera.destroy();
-				renderCamera = new Camera(this.width, this.height);
-			}
-		} else {
+		if (renderCamera == null) {
 			renderCamera = new Camera(this.width, this.height);
 		}
-		renderCamera.scale = resolution;
+		renderCamera.scale = this.scale;
 
 		renderBake = renderUpdate = true;
+		this.flushDrawables = flashDrawables;
 	}
 
 	override function render(paintMode:PaintMode, transform:FastMatrix4) {
@@ -49,14 +51,18 @@ class BakeLayer extends Layer {
 				GEngine.i.beginCanvas();
 				var lastCamera = paintMode.camera;
 				paintMode.camera = renderCamera;
-				super.render(paintMode, FastMatrix4.translation(-renderCamera.width * 0.5, -renderCamera.height * 0.5, 0).multmat(FastMatrix4.scale(scale,
-					scale, 1)));
-				paintMode.render(true);
+
+				super.render(paintMode,FastMatrix4.translation(-renderCamera.width * 0.5, -renderCamera.height * 0.5, 0).multmat(FastMatrix4.scale(scale, scale, 1)));
+			//	super.render(paintMode,renderCamera.view);
+				paintMode.render();
 				paintMode.camera = lastCamera;
 				GEngine.i.endCanvas();
 				GEngine.i.setCanvas(lastTarger);
 				GEngine.i.beginCanvas();
 				renderUpdate = false;
+				if (flushDrawables) {
+					this.children.splice(0, this.children.length);
+				}
 			}
 			paintMode.render();
 			renderBuffer(renderCamera.renderTarget, GEngine.i.getSimplePainter(BlendMode.Default), x, y, width, height, paintMode, transform);
@@ -71,24 +77,29 @@ class BakeLayer extends Layer {
 		var tex = GEngine.i.getTexture(source);
 		var texWidth = tex.realWidth;
 		var texHeight = tex.realHeight;
+		var invert=Image.renderTargetsInvertedY();
+		writeVertex(painter, x, y, 0, texWidth, texHeight, transform,invert);
 
-		writeVertex(painter, x, y, 0, texWidth, texHeight, transform);
+		writeVertex(painter, x + width, y, 0, texWidth, texHeight, transform,invert);
 
-		writeVertex(painter, x + width, y, 0, texWidth, texHeight, transform);
+		writeVertex(painter, x, y + height, 0, texWidth, texHeight, transform,invert);
 
-		writeVertex(painter, x, y + height, 0, texWidth, texHeight, transform);
-
-		writeVertex(painter, x + width, y + height, 0, texWidth, texHeight, transform);
+		writeVertex(painter, x + width, y + height, 0, texWidth, texHeight, transform,invert);
 
 		painter.render();
 	}
 
-	inline function writeVertex(painter:IPainter, x:Float, y:Float, z:Float, sWidth:Float, sHeight:Float, transform:FastMatrix4) {
+	inline function writeVertex(painter:IPainter, x:Float, y:Float, z:Float, sWidth:Float, sHeight:Float, transform:FastMatrix4,invert:Bool) {
 		var pos = transform.multmat(FastMatrix4.scale(1 / scale, 1 / scale, 1)).multvec(new FastVector4(x, y, z));
 		painter.write(pos.x);
 		painter.write(pos.y);
 		painter.write(pos.z);
 		painter.write(x / sWidth);
-		painter.write(y / sHeight);
+		if(invert){
+			painter.write((sHeight-y) / sHeight);
+		}else{
+			painter.write(y / sHeight);
+		}
+		
 	}
 }
